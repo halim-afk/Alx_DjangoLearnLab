4fa81f4d673796878274e07e6d8caaca2f62e861
halim-afk <a.alouah0243@uca.ac.ma>
2025-07-30 15:41:35 +0100

    fix bug

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -62,7 +62,9 @@ def run_queries():
 
     # List all books in a library
     print("\nList all books in Central City Library:")
-    central_library = Library.objects.get(name="Central City Library")
+    # Introduce a variable for the library name
+    library_name = "Central City Library" # <--- NEW LINE: Define the variable
+    central_library = Library.objects.get(name=library_name) # <--- MODIFIED LINE: Use the variable
     books_in_central_library = central_library.books.all()
     for book in books_in_central_library:
         print(f"- {book.title}")
