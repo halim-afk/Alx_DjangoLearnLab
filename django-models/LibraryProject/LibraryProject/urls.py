670dceb5be3926cb124e2e5bb2b1d942350d47f6
halim-afk <a.alouah0243@uca.ac.ma>
2025-07-31 23:54:31 +0100

    debug

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -4,4 +4,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
+    path('books/', list_books, name='list_books'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+    
 ]
