d53661ac333584f38f3ffac00aec38e4f8333aba
halim-afk <a.alouah0243@uca.ac.ma>
2025-07-31 12:52:22 +0100

    clean the workpace(new begain)

--- django-models/LibraryProject/relationship_app/admin.py
+++
@@ -1,8 +0,0 @@
-# relationship_app/admin.py
-from django.contrib import admin
-from .models import Author, Book, Library, Librarian # Import all your models
-
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Library)
-admin.site.register(Librarian)
--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView # Make sure these are imported directly
-
-app_name = 'relationship_app'
-
-urlpatterns = [
-    path('books/', list_books, name='book_list'), # Corrected line
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
