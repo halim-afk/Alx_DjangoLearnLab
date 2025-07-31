40a3b73b0a24c42edf916fba9d7f8b704e4f2865
halim-afk <a.alouah0243@uca.ac.ma>
2025-07-31 22:07:25 +0100

    debug

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,30 +1,19 @@
-from relationship_app.models import Author, Book, Library, Librarian
 from .models import Author, Book, Library, Librarian
 
-#Get all books by a specific author
+Get all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)  # <- هذا السطر مطلوب للفحص
-    return Book.objects.filter(author=author)      # <- وهذا أيضاً
+    # This function retrieves all books written by a given author name
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
-#Get all books in a library
+Get all books in a specific library
 def get_books_in_library(library_name):
+    # This function retrieves all books in the specified library
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-#Retrieve the librarian for a library
+Get the librarian of a specific library
 def get_librarian_of_library(library_name):
-    library = Library.objects.get(name=library_name)  # <- مطلوب أيضاً
-    return Librarian.objects.get(library=library)     # <- وهذا كذلك
-
-
-def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
-
-
-def books_in_library(library_name):
+    # This function retrieves the librarian assigned to the given library
     library = Library.objects.get(name=library_name)
-    return library.books.all()
-
-def librarian_of_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
