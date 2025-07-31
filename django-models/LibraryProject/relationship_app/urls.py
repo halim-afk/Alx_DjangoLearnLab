79899b5ef584c644cdf5a581d9cb0107d91c14fc
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-01 00:09:40 +0100

    debug

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+ from .views import    add_book, edit_book, delete_book
+
 from .views import (
     list_books, LibraryDetailView, RegisterView,
-    add_book, edit_book, delete_book,
     admin_view, librarian_view, member_view,
     register
 )
