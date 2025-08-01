a4eff0370a06bd36eda712557d85504141cc09c4
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-01 17:12:00 +0100

    debug

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book  # تأكد من أن نموذج Book معرف في models.py
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
 
 # Create your views here.
