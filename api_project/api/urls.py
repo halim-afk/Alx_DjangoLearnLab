304259cc7e5026313beb79a3024c98d94cc43010
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-03 17:29:53 +0100

    add security configurations

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,10 +4,18 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet, BookList
 
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book')
+
+
+
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('token/', obtain_auth_token),  # endpoint لإصدار التوكن
     path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,15 @@ from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
+
 #ViewSet for full CRUD operations
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # الصلاحيات
+
 
 #ListAPIView for basic book list
 class BookList(generics.ListAPIView):
