b553cc3a572ddfac9414e7d822e66f5ab8d47a44
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-03 16:30:46 +0100

    debug

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics.ListAPIView import ListAPIView
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
 class BookList(ListAPIView):
