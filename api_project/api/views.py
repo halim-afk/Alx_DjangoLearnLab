1d88fab60c96630112906b15fb4b18ba1b44f7b8
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-03 16:34:17 +0100

    debug

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,7 +9,7 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
 
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
