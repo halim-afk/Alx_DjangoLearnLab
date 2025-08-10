138ab3bd645bbc04244b095039f01ae6acc79b89
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-10 21:09:41 +0100

    add featurs

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,25 +2,27 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AuthorViewSet, BookViewSet
 
+
 from .views import (
-    BookListView, BookDetailView,
-    BookCreateView, BookUpdateView, BookDeleteView
+    BookListView, BookDetailView, BookCreateView,
+    BookUpdateView, BookDeleteView,
+    AuthorViewSet, BookViewSet
 )
 
-urlpatterns = [
-    path('', include(router.urls)),
-
-    # Generic Views for Book
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
-]
-
-
-# Router automatically creates API routes
+# Router for ViewSets
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet)
 router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    # Generic Views for Books
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+
+    # ViewSets endpoints
+    path('', include(router.urls)),
+]
 
