f971def66c64976b55731845332b42d8d3e28478
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-17 11:25:30 +0100

    fix some bugs

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -18,12 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views import UserLoginView, register ,profile ,UserLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # include app urls
-
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
 ]
+
+
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
