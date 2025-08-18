8b2bfdc3dc9deec1028ee57da8c1afecaeed5c88
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-18 12:28:21 +0100

    debug

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,11 +2,30 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from .models import User
-class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True) # This is a CharField
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)
+        return user
+
+
+
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
