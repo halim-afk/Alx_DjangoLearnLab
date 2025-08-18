9e2c37ae15d09ad444a51972def47ed56251cffd
halim-afk <a.alouah0243@uca.ac.ma>
2025-08-18 13:26:41 +0100

    add social_media_api derectory

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import UserRegistrationView, UserLoginView, UserProfileView, FollowUserView, UnfollowUserView
 from rest_framework_simplejwt.views import TokenRefreshView
-
+from .views import FollowUserView, UnfollowUserView
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('profile/<str:username>/', UserProfileView.as_view(), name='user_profile'),
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
     path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
